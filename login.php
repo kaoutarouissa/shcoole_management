3342334ec74ae31ef368e3f801173a82289b8ced
kaoutar <kaoutar@gmail.com>
2026-04-23 12:52:54 +0100

    aficher msg derreur

---
+++ login.php
@@ -0,0 +1,49 @@
+<?php
+include("connection.php");
+
+$email_error = "";
+$pass_error = "";
+
+if (isset($_POST['submit'])) {
+    $email = "";
+    $password = "";
+
+    if (isset($_POST['email'])) {
+        $email = trim($_POST['email']);
+    }
+
+    if (isset($_POST['password'])) {
+        $password = $_POST['password'];
+    }
+
+    if ($email == "") {
+        $email_error = "Enter email";
+    }
+
+    if ($password == "") {
+        $pass_error = "Enter password";
+    }
+
+    if ($email_error != "" || $pass_error != "") {
+        include("index.php");
+        exit();
+    }
+
+    $email = mysqli_real_escape_string($conn, $email);
+    $sql = "SELECT * FROM register WHERE email='$email'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        $user = mysqli_fetch_assoc($result);
+
+        if (password_verify($password, $user['password'])) {
+            include("welcome.php");
+            exit();
+            // echo "Login success. Welcome " . $user['username'];
+        } 
+    
+    } else {
+        echo "User not found";
+    }
+    }
+?>
