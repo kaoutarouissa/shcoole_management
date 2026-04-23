2ccf44b2c09e536e335292bf00eb08bc83d244fd
kaoutar <kaoutar@gmail.com>
2026-04-23 11:55:26 +0100

    verification d'exestence du username  et email dans la base de donees

--- register_post.php
+++ register_post.php
@@ -1,67 +1,75 @@
 <?php
-include ('connection.php');
+include('connection.php');
+
+$err = 0;
+
 if(isset($_POST['submit'])){
-    $username=stripslashes( $_POST['name']);
-    $email=stripslashes( $_POST['email']);
-    $password=stripcslashes( $_POST['password']);
-    $confirm_password=stripslashes($_POST['confirm_password']);
 
+    $username = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
 
-$username=htmlentities(mysqli_real_escape_string($conn,$_POST['name']));
-$email=htmlentities(mysqli_real_escape_string($conn,$_POST['email']));
-$password=htmlentities(mysqli_real_escape_string($conn,$_POST['password']));
-$confirm_password=htmlentities(mysqli_real_escape_string($conn,$_POST['confirm_password']));
+ $check_user="SELECT * FROM `register` WHERE username='$username'";
+ $check_result=mysqli_query($conn,$check_user) ;
+ $num_rows=mysqli_num_rows($check_result);
 
-if(empty($username)){
-    $user_error='enter username';
+ $check_email="SELECT * FROM `register` WHERE email='$email'";
+ $check_resultemail=mysqli_query($conn,$check_email);
+ $email_rows=mysqli_num_rows($check_resultemail);
+
+if($num_rows != 0){
+    $user_error="already username exist";
     $err=1;
-    }elseif(strlen($username)<6){
-        $user_error='enter usernnam minm 6 letters <br>';
-        $err=1;
-        }elseif(filter_var($username,FILTER_VALIDATE_INT)){
-            $user_error='enter  A  coorect validate username';
-  $err=1;
-  }
+}
 
-
-  
-  if(empty($email)){
-    $email_error='enter your email';
+if($email_rows != 0){
+    $email_error="email already exist";
     $err=1;
-    }elseif(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-        $email_error='enter coorect valide email  <br>';
-       $err=1;
-       
-       }
+}
 
 
+    if(empty($username)){
+        $user_error = "Enter username";
+        $err = 1;
+    } elseif(strlen($username) < 6){
+        $user_error = "Username must be at least 6 characters";
+        $err = 1;
+    }
 
 
-       if($password !== $confirm_password){
-                    $pass_error='PASS WORD ISNT CONFIRMED <br>';
+    if(empty($email)){
+        $email_error = "Enter email";
+        $err = 1;
+    } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $email_error = "Invalid email format";
+        $err = 1;
+    }
 
-        } else {
-       
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-       }
-         if(empty($password)){
-    $pass_error='enter your password';
-    $err=1;
-    }elseif(strlen($password)<6){
-        $pass_error='enter coorect valide passowrd min 6 letters <br>';
-       $err=1;
-       include('registerForm.php');
-       
-       }
-       else{
-        if($err =0){
-            $sql="INSERT INTO register(username,email,password,id) values('$username','$email','$hashed_password',) " ;
-            mysqli_query($conn,$sql);
-            header('location:index.php');
+    
+    if(empty($password)){
+        $pass_error = "Enter password";
+        $err = 1;
+    } elseif(strlen($password) < 6){
+        $pass_error = "Password must be at least 6 characters";
+        $err = 1;
+    } elseif($password !== $confirm_password){
+        $pass_error = "Passwords do not match";
+        $err = 1;
+    } else {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    }
 
-        }else{
-            include('registerForm.php');
-        }
-       }
-       }
-       ?>
+
+    if(($err == 0) && ($num_rows == 0)){
+        $sql = "INSERT INTO register (username, email, password)
+                VALUES ('$username', '$email', '$hashed_password')";
+
+        mysqli_query($conn, $sql);
+        header('Location: index.php');
+        exit();
+    } else {
+        include('registerForm.php');
+    }
+}
+?>
